34b523bb9eca472c9f65c1e1efacaf59fb42b04e
Tom Exceed <nikita.exceed@gmail.com>
2020-06-03 16:58:42 +0300

    header design

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,26 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 export class Header extends Component {
     render() {
         return (
-            <div>
-                <Link to="/">Home</Link><span> </span>
-                <Link to="/login">Login</Link><span> </span>
-                <Link to="/registration">Registration</Link>
+            <div className="header">
+                <Link className="navButton" to="/">
+                    <Button variant="contained" size="small" color="primary">
+                        Home
+                    </Button>
+                </Link>
+                <Link className="navButton" to="/login">
+                    <Button variant="contained" size="small" color="primary">
+                        Login
+                    </Button>
+                </Link>
+                <Link className="navButton" to="/registration">
+                    <Button variant="contained" size="small" color="primary">
+                        Registration
+                    </Button>
+                </Link>
             </div>
         )
     }
